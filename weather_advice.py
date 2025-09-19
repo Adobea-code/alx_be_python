2adc64eb740066c2cdbe07ea1690dcec9a59b9b6
Adobea-code <adobea015@gmail.com>
2025-09-19 15:58:59 +0000

    Add files via upload

---
+++ weather_advice.py
@@ -0,0 +1,15 @@
+weather= (input("What is the weather today?: " ))
+sunny= "sunny"
+rainy= "rainy"
+cold= "cold"
+
+if weather is sunny:
+    print("Wear a t-shirt and sunglasses ")
+elif weather is rainy:
+    print ("Do not forget your umbrella and a raincoat")
+elif weather is cold:
+    print("Make sure to wear a warm coat and a scarf")
+else:
+    print ("Error")
+
+
